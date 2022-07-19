5d3173dd829a798b3d2733d335d23cebd60462cd
unknown <shivasundar911@gmail.com>
2022-07-19 12:22:32 +0530

    form added

--- myworld/user/views.py
+++ myworld/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.db import models
 
+from .forms import userForm
+from .models import User
 # Create your views here.
+
+def userView(request):
+    form = userForm(request.POST or None)
+    if(form.is_valid()):
+        form.save()
+        form = userForm()
+
+    context = {
+        'form': form
+    }
+    
+    return render(request, "create_user.html",context)

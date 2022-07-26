a9f20ed316e439a46088e5435d1a3a96daa8c7df
Sneha <snehamathew2002@gmail.com>
2022-07-26 13:28:48 +0530

    added signup url

--- myworld/myworld/urls.py
+++ myworld/myworld/urls.py
@@ -3,12 +3,15 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView # new
 from user.views import userView
 from user.views import front
+from signup.views import signupView, front
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', TemplateView.as_view(template_name='login.html'), name='login'), # new
     path('create/', userView, name="create user"),
+
+    path('signup/', signupView, name = "register user"),
     
     path("", front, name="frontend")
     # path('',TemplateView.as_view(template_name='index.html'), name = "index.html"),

bc14717cbaf46168ccb81db5199eadf116c20f0d
Sneha <snehamathew2002@gmail.com>
2022-07-26 13:29:19 +0530

    created signup view

--- myworld/signup/views.py
+++ myworld/signup/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.db import models
+
+from .forms import userForm
+from .models import User
 
 # Create your views here.
+
+def signupView(request):
+    form = userForm(request.POST or None)
+    if (form.ins_valid()):
+        form.save()
+        form = userForm()
+    
+    context = {
+        'form' : form
+    }
+
+    return render(request, "create_user.html",context)

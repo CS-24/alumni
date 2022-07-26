923cc03f6fbacfb92d516eab6f097b4e3fd04a98
Sneha <snehamathew2002@gmail.com>
2022-07-26 13:07:29 +0530

    created user model

--- myworld/signup/models.py
+++ myworld/signup/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.TextField(null=True)
+    age = models.DecimalField(decimal_places=0, max_digits=3,null=True, blank=True)
+    phone = models.DecimalField(decimal_places= 0, max_digits=10)
+    email = models.TextField(null=True)
